a877f5fefde9837e25814cf59b58c0825c39209a
Nathan Johnstone <locotewz@gmail.com>
2016-09-08 20:33:48 +0200

    versioning

--- src/AppVeyorExperiments/Properties/AssemblyInfo.cs
+++ src/AppVeyorExperiments/Properties/AssemblyInfo.cs
@@ -17,5 +17,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3739e601-9b5d-4c26-9664-4c50e779e698")]
-[assembly: AssemblyFileVersion("0.0.1")]
-[assembly: AssemblyVersion("0.0.1")]
