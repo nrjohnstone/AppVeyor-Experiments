ccca296b64b8763a4e4645bb71826ba9ea800dce
Nathan Johnstone <locotewz@gmail.com>
2016-09-08 11:00:54 +0200

    initial test solution

---
+++ src/AppVeyorExperiment/FooManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppVeyorExperiment
+{
+    public class FooManager
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ src/Tests/FooManagerTests.cs
@@ -0,0 +1,18 @@
+﻿using AppVeyorExperiment;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppVeyorExperiments.Tests
+{
+    [TestClass]
+    public class FooManagerTests
+    {
+        [TestMethod]
+        public void Add_TwoNumbers_ShouldReturnSum()
+        {
+            var sut = new FooManager();
+
+            sut.Add(2, 3).Should().Be(5);
+        }
+    }
+}
